68a79de573ed1e200ca9c768284866144893e31e
Dungdd <daoducdung@ginno.com>
2022-11-28 12:05:23 +0700

    Soft Delete

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator')
+const mongooseDelete = require('mongoose-delete')
 const Schema = mongoose.Schema;
-
-mongoose.plugin(slug);
 
 const Course = new Schema({
     name: { type: String, required: true },
@@ -12,7 +11,14 @@ const Course = new Schema({
     level: { type: String, maxLength: 255 },
     slug: { type: String, slug: 'name', unique: true},
 }, {
-    timestamps: true
+    timestamps: true,
 });
+
+// Add plugin
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: "all",
+})
 
 module.exports = mongoose.model('Course', Course);
